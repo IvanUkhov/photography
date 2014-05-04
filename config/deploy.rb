be76579ec8ca31ad4aabff6fba4d798dd0183d1a
Ivan Ukhov <ivan.ukhov@gmail.com>
2014-05-04 07:42:18 +0200

    Made Capistrano restart the server

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,14 +1,15 @@
 set :application, 'photography'
 set :repo_url, 'gitolite@ukhov.me:web/photography.git'
+set :keep_releases, 2
 
 namespace :deploy do
   desc 'Restart application'
   task :restart do
-    on roles(:app), in: :sequence, wait: 5 do
+    on roles(:all), in: :sequence, wait: 5 do
       execute :touch, release_path.join('tmp/restart.txt')
     end
   end
-end
 
-after :deploy, 'deploy:cleanup'
-after :deploy, 'deploy:restart'
+  after :publishing, :cleanup
+  after :publishing, :restart
+end
