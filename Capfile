ce0c89d182569fa73e2658f09f4e95705706274d
Ivan Ukhov <ivan.ukhov@gmail.com>
2014-06-21 22:21:25 +0200

    Removed Capistrano’s stuff

--- Capfile
+++
@@ -1,6 +0,0 @@
-require 'capistrano/setup'
-require 'capistrano/deploy'
-require 'capistrano/bundler'
-require 'capistrano/rvm'
-
-set :stage, :production
--- config/deploy/production.rb
+++
@@ -1,6 +0,0 @@
-set :stage, :production
-
-role :web, %w{ivan@ukhov}
-server 'ukhov', user: 'ivan', roles: %w{web app}
-
-set :deploy_to, '/home/ivan/projects/photography'
